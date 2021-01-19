90eff8f4c228256d1d3f8b12f015492a63fbe288
ronaldvbostelen <43178834+ronaldvbostelen@users.noreply.github.com>
2021-01-19 17:50:25 +0100

    Add services

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import {HeroService} from '../services/hero.service';
+import { MessageService } from '../services/message.service';
+
+import { Hero } from '../models/hero';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesCompoent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(x => this.heroes = x);
+  }
+}
